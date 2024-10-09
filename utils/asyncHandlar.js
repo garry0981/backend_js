bc0d76e8e9816cb5ef75a27cdbdbab335b260f06
garry0981 <gauravgarry6@gmail.com>
2024-10-09 03:30:55 +0000

    util code disscussion

---
+++ utils/asyncHandlar.js
@@ -0,0 +1,24 @@
+const asyncHandlar = (requestHandlar) =>{
+    (req,res,next) =>{
+        Promise.resolve(requestHandlar(req,res,next)).catch((err) => next(err))
+    }
+}
+
+
+export {asyncHandlar}
+
+
+// const asyncHandlar=() =>{}
+// const asyncHandlar=(func) =>() =>{}
+// const asyncHandlar = (func) =>async() =>{}
+
+// const asyncHandlar =(fn) => async(req,res,next) =>{
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success:false,
+//             massage:err.massage
+//         })
+//     }
+// }
